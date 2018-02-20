6ec63c98207f74266eeba2c0daff0bfd846b1a51
Luis Fernando Rodrigues <luisfernandorodrigues@luiss-macbook-pro.local>
2018-02-19 21:03:11 -0300

    Initial Commit

---
+++ IMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Luis Fernando Rodrigues  on 2/19/18.
+//  Copyright © 2018 Luis Fernando Rodrigues . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
